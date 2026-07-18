37494e115898d794d765fd20251c20fe40dc0ce3
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2026-07-19 01:35:32 +0300

    Initial Commit

---
+++ ketum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ketum
+//
+//  Created by Abdullah Faruk Çiftler on 19.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ketum/ketumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ketumApp.swift
+//  ketum
+//
+//  Created by Abdullah Faruk Çiftler on 19.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ketumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
